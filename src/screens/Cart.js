97e77eeb26cbd5693e95d4d60368fb1dcbb521f2
iqbal <emixbal@gmail.com>
2019-05-13 00:33:10 +0700

    add fetch data with global API

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -2,10 +2,30 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
 import {GlobalConsumer} from '../context.js';
+import API from '../services/index.js';
 
 class Cart extends Component{
+    state = {
+        products:[],
+        isLoading: true
+    }
+    componentDidMount(){
+        API.getProducts()
+        .then(result=>{
+            console.log(result.products);
+            
+            this.setState({
+                products:result,
+                isLoading:false
+            })
+        })
+    }
+
     render(){
         const { totalOrder } = this.props.state
+        if(this.state.isLoading===true){
+            return <div>Loading...</div>
+        }
         return(
             <div>
                 {totalOrder}
@@ -14,7 +34,15 @@ class Cart extends Component{
                 <hr />
                 <button onClick={()=>this.props.dispatch({type:"MINUS"})}>kurang</button>
                 <hr />
+                <ul>
+                    {
+                        this.state.products.products.map(product => (
+                            <li key={product.id}>{product.name}</li>
+                        ))
+                    }
+                </ul>
                 <Link to="/">HOME</Link>
+                
             </div>
         );
     }
